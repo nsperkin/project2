9cf9a71bd959f40dcafcfa05f021abe958f431d9
Nathaniel Perkins <nsperkins13@gmail.com>
2019-07-16 11:40:40 -0700

    Everything working except messges
    Error with socketio

--- application.py
+++ application.py
@@ -1,12 +1,21 @@
 import os
 
-from flask import Flask, render_template, session, redirect, url_for
+from flask import Flask, render_template, session, redirect, url_for, request
+from flask_session import Session
 from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 socketio = SocketIO(app)
 
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+channels = {}
+
+if __name__ == '__main__':
+	socketio.run(app)
 
 @app.route("/")
 def index():
@@ -15,6 +24,47 @@ def index():
 	else:
 		return render_template('index.html')
 
+@app.route("/login", methods=["POST"])
+def login():
+	session['displayname'] = request.form.get("displayname")
+	session['lastChannel'] = ""
+	return redirect(url_for('home'))
+
+@app.route("/logout")
+def logout():
+	session.pop('displayname', None)
+	return render_template("index.html")
+
 @app.route("/home")
 def home():
-	return render_template('home.html')
+	if 'displayname' in session:
+		if session['lastChannel'] != "":
+			return redirect('channel', name=session['lastChannel'])
+		else:
+			return render_template('home.html', chan=channels, message="No Channel Selected!")
+	else:
+		return render_template('index.html')
+
+@app.route("/newChannel", methods=["POST"])
+def newChannel():
+	channel = request.form.get("channel")
+	channels.update({channel:[]})
+
+	return redirect(url_for('channel', name=channel))
+
+
+@app.route("/channel/<string:name>")
+def channel(name):
+	if "displayname" in session:
+		session['lastChannel'] = name
+		return render_template('home.html', chan=channels, message=name)
+	else:
+		return render_template('index.html')
+
+@socketio.on('submit message')
+def messsage(data):
+	message = data["message"]
+	print("================================")
+	print(message)
+	displayname = session["displayname"]
+	emit("announce message", {"message": message, "displayname": displayname}, broadcast=True)
--- static/script.js
+++ static/script.js
@@ -1,8 +1,21 @@
 document.addEventListener('DOMContentLoaded', () => {
-	document.querySelector('#expandbutton').onclick = openNav;
+	document.querySelector('#expandbutton').onclick = closeNav;
+	document.querySelector('#add').onclick = addChannel;
+
+	var socket = io.connect(location.protocol + '//' + document.domain + ':' + location.port);
+	socket.on('connect', () => {
+		document.querySelector('#message').onSubmit = () => {
+			const message = document.querySelector('#mes').value;
+			socket.emit('submit message', {'message': message})
+		};
 	});
 
-
+	socket.on('announce message', data => {
+		const div = document.createElement('div');
+        div.innerHTML = `${data.displayname}: ${data.selection}`;
+        document.querySelector('#messages').append(div);
+    });
+});
 
 function openNav() {
   document.getElementById("mySidenav").style.width = "250px";
@@ -14,4 +27,8 @@ function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
   document.getElementById("main").style.marginLeft= "0";
   document.querySelector('#expandbutton').onclick = openNav;
+}
+
+function addChannel() {
+	document.querySelector('#newChannel').style.display = 'initial';
 }
